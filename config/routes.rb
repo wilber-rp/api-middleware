0ff639b372ae4af00d0c7d39352007734c54a0cc
Marcos Flach <marcosoftn1@gmail.com>
2024-04-12 21:42:05 -0300

    Folder structure finished

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      ####
+    end
+  end
 end
