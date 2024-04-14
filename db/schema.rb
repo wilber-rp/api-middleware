0fdfff3ad4cfb21f9d8c9373cefe238b2cbdf6c4
Marcos Flach <marcosoftn1@gmail.com>
2024-04-14 17:54:45 -0300

    validation completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_13_200007) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_193314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_200007) do
     t.bigint "payment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "no_invoice"
     t.index ["payment_id"], name: "index_orders_on_payment_id"
     t.index ["source_id"], name: "index_orders_on_source_id"
   end
