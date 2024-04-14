5a6d4498d17fec2662ca043f449bc10d00f9586e
Marcos Flach <marcosoftn1@gmail.com>
2024-04-14 19:57:21 -0300

    Parse in progress

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   def index
     @orders = Order.all
   end
-
+  
   def show
     @order = Order.find(params[:id])
   end
@@ -15,6 +15,31 @@ class Api::V1::OrdersController < Api::V1::BaseController
     @order.payment_id = 1
     @order.source_id = 1
     if @order.save
+      url = "https://publica.cnpj.ws/cnpj/#{@order.cnpj}"
+      json_data = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
+      if json_data['status'] != 429
+        @order.ie = json_data["estabelecimento"]["inscricoes_estaduais"].first["inscricao_estadual"]
+        @order.address = "#{json_data['estabelecimento']['tipo_logradouro']} #{json_data['estabelecimento']['logradouro']}"
+        @order.number = json_data['estabelecimento']['numero']
+        @order.complement = json_data['estabelecimento']['complemento']
+        @order.neighborhood = json_data['estabelecimento']['bairro']
+        @order.city = json_data['estabelecimento']['cidade']['nome']
+        @order.state = json_data['estabelecimento']['estado']['sigla']
+        @order.zip_code = json_data['estabelecimento']['cep']
+      else
+        sleep(60)
+        json_data = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
+        @order.ie = json_data["estabelecimento"]["inscricoes_estaduais"].first["inscricao_estadual"]
+        @order.address = "#{json_data['estabelecimento']['tipo_logradouro']} #{json_data['estabelecimento']['logradouro']}"
+        @order.number = json_data['estabelecimento']['numero']
+        @order.complement = json_data['estabelecimento']['complemento']
+        @order.neighborhood = json_data['estabelecimento']['bairro']
+        @order.city = json_data['estabelecimento']['cidade']['nome']
+        @order.state = json_data['estabelecimento']['estado']['sigla']
+        @order.zip_code = json_data['estabelecimento']['cep']
+      end
+
+      @order.save!
       render :show, status: :created
     else
       render_error
@@ -33,7 +58,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   private
 
   def order_params
-    params.require(:order).permit(:order_number, :address, :model, :deceased_name,:costumer_full_name, :cpf, :company_name, :cnpj, :ie, :address, :number, :complement, :neighborhood, :city, :state, :zip_code, :email, :phone_number )
+    params.require(:order).permit(:order_number, :address, :model, :deceased_name,:costumer_full_name, :cpf, :company_name, :cnpj, :ie, :address, :number, :complement, :neighborhood, :city, :state, :zip_code, :email, :phone_number, :no_invoice )
   end
 
   def render_error
