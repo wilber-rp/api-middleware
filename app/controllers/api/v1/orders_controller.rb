190535244cea6007bf4758a01e6c8a757dabd2c0
Marcos Flach <marcosoftn1@gmail.com>
2024-04-14 23:46:04 -0300

    refactored method update_company

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   def index
     @orders = Order.all
   end
-  
+
   def show
     @order = Order.find(params[:id])
   end
@@ -15,31 +15,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
     @order.payment_id = 1
     @order.source_id = 1
     if @order.save
-      url = "https://publica.cnpj.ws/cnpj/#{@order.cnpj}"
-      json_data = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
-      if json_data['status'] != 429
-        @order.ie = json_data["estabelecimento"]["inscricoes_estaduais"].first["inscricao_estadual"]
-        @order.address = "#{json_data['estabelecimento']['tipo_logradouro']} #{json_data['estabelecimento']['logradouro']}"
-        @order.number = json_data['estabelecimento']['numero']
-        @order.complement = json_data['estabelecimento']['complemento']
-        @order.neighborhood = json_data['estabelecimento']['bairro']
-        @order.city = json_data['estabelecimento']['cidade']['nome']
-        @order.state = json_data['estabelecimento']['estado']['sigla']
-        @order.zip_code = json_data['estabelecimento']['cep']
-      else
-        sleep(60)
-        json_data = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
-        @order.ie = json_data["estabelecimento"]["inscricoes_estaduais"].first["inscricao_estadual"]
-        @order.address = "#{json_data['estabelecimento']['tipo_logradouro']} #{json_data['estabelecimento']['logradouro']}"
-        @order.number = json_data['estabelecimento']['numero']
-        @order.complement = json_data['estabelecimento']['complemento']
-        @order.neighborhood = json_data['estabelecimento']['bairro']
-        @order.city = json_data['estabelecimento']['cidade']['nome']
-        @order.state = json_data['estabelecimento']['estado']['sigla']
-        @order.zip_code = json_data['estabelecimento']['cep']
-      end
-
-      @order.save!
+      update_company(@order.id, @order.cnpj)
       render :show, status: :created
     else
       render_error
@@ -49,10 +25,34 @@ class Api::V1::OrdersController < Api::V1::BaseController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      update_company(@order.id, @order.cnpj)
       render :show
     else
       render_error
     end
+  end
+
+  def update_company(order_id, cnpj)
+    order = Order.find_by(id: order_id)
+    url = "https://publica.cnpj.ws/cnpj/#{cnpj}"
+    json_data = HTTParty.get(url, :headers =>{'Content-Type' => 'application/json'})
+    if json_data['status'] != 429
+      order.company_name = json_data['razao_social']
+      order.address = "#{json_data['estabelecimento']['tipo_logradouro']} #{json_data['estabelecimento']['logradouro']}"
+      order.number = json_data['estabelecimento']['numero']
+      order.complement = json_data['estabelecimento']['complemento']
+      order.neighborhood = json_data['estabelecimento']['bairro']
+      order.city = json_data['estabelecimento']['cidade']['nome']
+      order.state = json_data['estabelecimento']['estado']['sigla']
+      order.zip_code = json_data['estabelecimento']['cep']
+      if json_data["estabelecimento"].present? && json_data["estabelecimento"]["inscricoes_estaduais"].present?
+        order.ie = json_data["estabelecimento"]["inscricoes_estaduais"].first["inscricao_estadual"]
+      end
+    else
+      sleep(60)
+      update_company(order, cnpj)
+    end
+    order.save!
   end
 
   private
