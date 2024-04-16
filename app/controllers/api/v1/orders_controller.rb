3fee0331cbcab15e2d404b0cd2b583d41598efbb
Marcos Flach <marcosoftn1@gmail.com>
2024-04-16 01:29:17 -0300

    Token validator finished

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::OrdersController < Api::V1::BaseController
+  before_action :permissions_scope
+
   def index
     @orders = Order.all
   end
@@ -35,8 +37,25 @@ class Api::V1::OrdersController < Api::V1::BaseController
     params.require(:order).permit(:order_number, :address, :model, :deceased_name,:costumer_full_name, :cpf, :company_name, :cnpj, :ie, :address, :number, :complement, :neighborhood, :city, :state, :zip_code, :email, :phone_number, :no_invoice )
   end
 
+  def permissions_scope
+    if request.method == 'GET' && (request.headers['xkey'] != ENV['READ_ONLY'] && request.headers['xkey'] != ENV['ADMIN'])
+      render_no_access
+    end
+    if request.method == 'POST' && (request.headers['xkey'] != ENV['POST_ONLY'] && request.headers['xkey'] != ENV['ADMIN'])
+      render_no_access
+    end
+    if request.method == 'PATCH' && request.headers['xkey'] != ENV['ADMIN']
+      render_no_access
+    end
+  end
+
   def render_error
     render json: { errors: @order.errors.full_messages },
       status: :unprocessable_entity
+  end
+
+  def render_no_access
+    render json: { error: "Sem permissão para realizar #{request.method}" },
+    status: :unprocessable_entity
   end
 end
